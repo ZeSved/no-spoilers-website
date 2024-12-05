a9b262b346257cd0e38b6da55a5bfdf7dc5a0c50
Timothy Korell <timothy.korell@gmail.com>
2024-12-05 14:22:11 +0100

    message sent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 const hideSpoilers: string[] = []
 
 function App() {
 	const [spoilers, setSpoilers] = useState(hideSpoilers)
+	const iframeRef = useRef<HTMLIFrameElement>(null)
 	// useEffect(() => {
 	// 	setTimeout(() => {
 	// 		document.body.querySelectorAll('ytd-video-renderer').forEach((item) => {
@@ -14,8 +15,23 @@ function App() {
 	// }, [])
 
 	useEffect(() => {
-		localStorage.setItem('no-spoilers-config', JSON.stringify(spoilers))
+		if (spoilers.length > 0) {
+			localStorage.setItem('no-spoilers-config', JSON.stringify(spoilers))
+		}
+		// if ((JSON.parse(localStorage.getItem('no-spoilers-config') ?? '[]') as string[]).length === 0) {
+		// } else return
+		// iframeRef.current!.contentWindow?.postMessage(
+		// 	JSON.stringify(localStorage.getItem('no-spoilers-config')),
+		// 	'https://www.youtube.com/embed/'
+		// )
 	}, [spoilers])
+
+	useEffect(() => {
+		setSpoilers(JSON.parse(localStorage.getItem('no-spoilers-config') ?? '[]'))
+		// localStorage.setItem('no-spoilers-config', JSON.stringify(spoilers))
+		// if ((JSON.parse(localStorage.getItem('no-spoilers-config') ?? '[]') as string[]).length === 0) {
+		// } else return
+	}, [])
 
 	return (
 		<main>
@@ -27,6 +43,11 @@ function App() {
 				<button
 					onClick={() => {
 						setSpoilers([...spoilers, (document.getElementById(`inp`) as HTMLInputElement).value])
+
+						// iframeRef.current!.contentWindow?.postMessage(
+						// 	JSON.stringify(localStorage.getItem('no-spoilers-config')),
+						// 	'https://www.youtube.com/embed/'
+						// )
 					}}>
 					done
 				</button>
@@ -37,6 +58,20 @@ function App() {
 					))
 				} */}
 			</div>
+			<iframe
+				ref={iframeRef}
+				src='https://www.youtube.com/embed/'
+				onLoad={() => {
+					setTimeout(() => {
+						iframeRef.current?.contentWindow?.postMessage(
+							// JSON.stringify(localStorage.getItem('no-spoilers-config')),
+							// ['test', 'yo'],
+							// JSON.parse(localStorage.getItem('no-spoilers-config') ?? 'null'),
+							'this is a test',
+							'https://www.youtube.com/*'
+						)
+					}, 5000)
+				}}></iframe>
 		</main>
 	)
 }
